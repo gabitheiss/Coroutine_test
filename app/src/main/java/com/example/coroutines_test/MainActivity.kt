58efd1712d0b7d6ba7ee682c2d5f3ac32b283dc5
gabitheiss <t_gaaby@hotmail.com>
2021-09-09 09:39:16 -0300

    Delay de 2s entre os nomes da lista

--- app/src/main/java/com/example/coroutines_test/MainActivity.kt
+++ app/src/main/java/com/example/coroutines_test/MainActivity.kt
@@ -2,27 +2,32 @@ package com.example.coroutines_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.coroutines_test.databinding.MainActivityBinding
 import com.example.coroutines_test.ui.main.MainFragment
 import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding : MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
-        CoroutineScope(Dispatchers.Default).launch {
+        CoroutineScope(Dispatchers.Main).launch {
             val retorno = callServer()
             retorno.forEach {
-                println(it)//retorna os nomes
+                binding.textView.text = it
+                delay(2000)
             }
         }
     }
 
     suspend fun callServer(): List<String> {
         println("...antes de disparar a coroutine...")
-        val listOfNames = withContext(Dispatchers.Default) {
+        val listOfNames = withContext(Dispatchers.Main) {
             delay(3000)
             listOf("Arthur", "Botão", "Jaime", "Leonardo")
         }
